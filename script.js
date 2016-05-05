40e8ad70b22b3b8eff27d7ede60b1a04fb4ade05
Hypnova <siavash2samiei@gmail.com>
2016-05-04 21:32:23 -0400

    Updated Scrip.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 $(function() {
-  $("head").append("<link rel=\"icon\" type=\"image/png\" href=\"../icon.png\">"); // Icon
-  $("head").append("<meta name=\"theme-color\" content=\"#268dbf\">"); // Chrome colour
-  $("#header").load("../header.html");
-  $("#footer").load("../footer.html");
+	$("head").append("<link rel=\"icon\" type=\"image/png\" href=\"../icon.png\">"); // Icon
+	$("head").append("<meta name=\"theme-color\" content=\"#268dbf\">"); // Chrome colour
+	$("#header").load("../header.html");
+	$("#footer").load("../footer.html");
+	
+	//Bootstrap core CSS
+	$("head").append("<link href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\" rel=\"stylesheet\">");
+	//MDB core CSS
+	$("head").append("<link href\"http://mdbootstrap.com/mdbcdn/mdb.min.css\" rel=\"stylesheet\">");   
+	//Bootstrap core JavaScript
+	$("head").append("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js\"></script>");
+	//MDB core JavaScript
+	$("head").append("<script src=\"http://mdbootstrap.com/mdbcdn/mdb.min.js\"></script>");
 });
+
