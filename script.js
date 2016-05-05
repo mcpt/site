14254a8990a1e06aef9c15a78377c13accc68b4a
aurpine <justinpu@rocketmail.com>
2016-05-04 22:23:45 -0400

    Fixed script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(function() {
 	//Bootstrap core CSS
 	$("head").append("<link href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\" rel=\"stylesheet\">");
 	//MDB core CSS
-	$("head").append("<link href\"http://mdbootstrap.com/mdbcdn/mdb.min.css\" rel=\"stylesheet\">");   
+	$("head").append("<link href=\"http://mdbootstrap.com/mdbcdn/mdb.min.css\" rel=\"stylesheet\">");
 	//Bootstrap core JavaScript
 	$("head").append("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js\"></script>");
 	//MDB core JavaScript
