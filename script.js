95802d1ccf2b837187e44bfa8e89815590e7d4b1
aurpine <justinpu@rocketmail.com>
2016-05-05 21:08:51 -0400

    Fixed the nav bar waves effect

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-$(function() {
-	$("head").append("<link rel=\"icon\" type=\"image/png\" href=\"../icon.png\">"); // Icon
+$(document).ready((function() {
 	$("head").append("<meta name=\"theme-color\" content=\"#3f51b5\">"); // Chrome colour
 	$("#header-replace").load("../header.html");
 	$("#footer-replace").load("../footer.html");
 	
 	//Bootstrap core CSS
-	$("head").append("<link href=\"../bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\">");
+	$("head").append("<link href=\"../bootstrap/css/bootstrap.css\" rel=\"stylesheet\">");
 	//MDB core CSS
-	$("head").append("<link href=\"../mdb/css/mdb.min.css\" rel=\"stylesheet\">");
+	$("head").append("<link href=\"../mdb/css/mdb.css\" rel=\"stylesheet\">");
 	//Bootstrap core JavaScript
-	$("head").append("<script src=\"../bootstrap/js/bootstrap.min.js\"></script>");
+	$("body").append("<script src=\"../bootstrap/js/bootstrap.js\"></script>");
 	//MDB core JavaScript
-	$("head").append("<script src=\"../mdb/js/mdb.min.js\"></script>");
-});
+	$("body").append("<script src=\"../mdb/js/mdb.js\"></script>");
+}));
 
