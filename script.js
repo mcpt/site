e4300be7af04df5fdd78ec8702e178c14db31349
aurpine <justinpu@rocketmail.com>
2016-05-06 23:55:25 -0400

    Performance fix

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 $(document).ready((function() {
-	$("head").append("<meta name=\"theme-color\" content=\"#5C6BC0\">"); // Chrome colour
+	$("head").append("<meta name=\"theme-color\" content=\"#C5CAE9\">"); // Chrome colour
 	$("#header-replace").load("../header.html");
 	$("#footer-replace").load("../footer.html");
-	
-	//Bootstrap core CSS
-	$("head").append("<link href=\"../bootstrap/css/bootstrap.css\" rel=\"stylesheet\">");
-	//MDB core CSS
-	$("head").append("<link href=\"../mdb/css/mdb.css\" rel=\"stylesheet\">");
-	//Bootstrap core JavaScript
-	$("body").append("<script src=\"../bootstrap/js/bootstrap.js\"></script>");
-	//MDB core JavaScript
-	$("body").append("<script src=\"../mdb/js/mdb.js\"></script>");
 }));
 
