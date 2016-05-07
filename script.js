d8ae2bbf286ca26a0e013f60dcbb9d911cc8ba1e
aurpine <justinpu@rocketmail.com>
2016-05-07 00:58:38 -0400

    Fixed the font and performance issues

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready((function() {
-	$("head").append("<meta name=\"theme-color\" content=\"#C5CAE9\">"); // Chrome colour
+	$("head").append("<meta name=\"theme-color\" content=\"#8C9EFF\">"); // Chrome colour
 	$("#header-replace").load("../header.html");
 	$("#footer-replace").load("../footer.html");
 }));
