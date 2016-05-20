c43089686ba5606dd9fc7512d6ccb3a61f79bb73
Justin Pu <aurpine@users.noreply.github.com>
2016-05-19 20:03:35 -0400

    Shorter Mathjax script

--- script.js
+++ script.js
@@ -2,8 +2,5 @@ $(document).ready(function() {
 	$("head").append("<meta name=\"theme-color\" content=\"#E8EAF6\">"); // Chrome colour
 	$("#header-replace").load("../header.html");
 	$("#footer-replace").load("../footer.html");
-	var mathjax = document.createElement("script");
-	mathjax.src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML";
-	mathjax.type = "text/javascript";
-	$("head").append(mathjax);
+	$("head").append("<script src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML\"></script>");
 });
