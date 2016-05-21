f478fe75140b4e0e4d0822e9e848c34f1faf18f6
aurpine <justinpu@rocketmail.com>
2016-05-21 13:42:31 -0400

    Updated theme-colour

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$("head").append("<meta name=\"theme-color\" content=\"#E8EAF6\">"); // Chrome colour
+	$("head").append("<meta name=\"theme-color\" content=\"#f48fb1\">"); // Chrome colour
 	$("#header-replace").load("../header.html");
 	$("#footer-replace").load("../footer.html");
 	$("head").append("<script src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML\"></script>");
