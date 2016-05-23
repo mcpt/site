000ecd12a9b49ccd9170a40476b27ab5dc38c4c2
aurpine <justinpu@rocketmail.com>
2016-05-23 00:19:42 -0400

    Updated links

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("head").append("<meta name=\"theme-color\" content=\"#f48fb1\">"); // Chrome colour
-	$("#header-replace").load("../header.html");
-	$("#footer-replace").load("../footer.html");
+	$("#header-replace").load("/header.html");
+	$("#footer-replace").load("/footer.html");
 	$("head").append("<script src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML\"></script>");
 });
