98498e203c3bc926d1abb35012e812ff7550c438
aurpine <justinpu@rocketmail.com>
2016-05-23 20:57:53 -0400

    Touching up

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
+  // Theme Colour
 	$("head").append("<meta name=\"theme-color\" content=\"#f48fb1\">"); // Chrome colour
+  // Add header
 	$("#header-replace").load("/header.html");
+  // Add footer
 	$("#footer-replace").load("/footer.html");
+  // Latex support
 	$("head").append("<script src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML\"></script>");
 });
