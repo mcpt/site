f7864a3ff1462ca9df15196c0a0a0d53e558676a
aurpine <justinpu@rocketmail.com>
2016-06-10 23:27:52 -0400

    New format

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
   // Add footer
 	$("#footer-replace").load("/footer.html");
   // Latex
+  $("head").append("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css\">");
 	$("head").append("<script src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_CHTML\"></script>");
   $("head").append("<script src=\"/ga.js\"></script>");
 });
